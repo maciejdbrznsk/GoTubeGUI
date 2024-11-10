52b705fc370862879afea6f28fce25b1b571928c
maciejdbrznsk <maciejdbrznsk@gmail.com>
2024-11-10 12:53:31 +0100

    optimized and fixed fuctions. Updated yt-dlp

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ import (
 	"os/user"
 	"path/filepath"
 	"regexp"
+	"sort"
 	"strconv"
 )
 
@@ -28,17 +29,33 @@ var ffmpegPath = "./ffmpeg"
 var downloadPath = getDefaultDownloadPath()
 
 type videoInfo struct {
-	Title       string   `json:"title"`
-	Description string   `json:"description"`
-	Thumbnail   string   `json:"thumbnail"`
-	Formats     []format `json:"formats"`
+	Title       string        `json:"title"`
+	Description string        `json:"description"`
+	Thumbnail   string        `json:"thumbnail"`
+	Formats     []ytDlpFormat `json:"formats"`
 }
 
-type format struct {
-	FormatID   string `json:"format_id"`
-	Resolution string `json:"resolution"`
-	FormatNote string `json:"format_note"`
-	Ext        string `json:"ext"`
+type Format struct {
+	ID         string
+	Resolution string
+	FormatNote string
+	Extension  string
+	AudioOnly  bool
+	Bitrate    int
+}
+
+type ytDlpFormat struct {
+	FormatID   string  `json:"format_id"`
+	Resolution string  `json:"resolution"`
+	Height     int     `json:"height"`
+	Width      int     `json:"width"`
+	Ext        string  `json:"ext"`
+	TBR        float32 `json:"tbr"` // total bitrate
+	ASR        int     `json:"asr"` // audio sample rate
+	FPS        float64 `json:"fps"`
+	VCodec     string  `json:"vcodec"`
+	ACodec     string  `json:"acodec"`
+	Filesize   int64   `json:"filesize"`
 }
 
 func main() {
@@ -80,7 +97,6 @@ func main() {
 		}
 	}()
 
-	// Dodaj listę wyboru formatu i rozdzielczości
 	formatSelect := widget.NewSelect([]string{"mp4", "mkv", "mp3", "wav"}, nil)
 	formatSelect.SetSelected("mp4")
 
@@ -107,11 +123,24 @@ func main() {
 			videoTitle.SetText("Title: " + videoInfo.Title)
 			videoDescription.SetText("Description: " + videoInfo.Description)
 
-			// Pobierz miniaturę i ustaw ją w interfejsie
-			thumbnail, err := fetchThumbnail(videoInfo.Thumbnail)
+			// Pobierz dostępne formaty
+			formats, err := getAvailableFormats(url)
+			if err != nil {
+				dialog.ShowError(err, myWindow)
+				return
+			}
+
+			// Użyj pobranych formatów do uzyskania listy rozdzielczości
+			resolutions := getUniqueResolutions(formats)
+			resolutionSelect.Options = resolutions
+			resolutionSelect.Refresh()
+
+			thumbnail, err := fetchAndConvertThumbnail(videoInfo.Thumbnail)
 			if err == nil {
 				thumbnailImage.Resource = thumbnail
 				thumbnailImage.Refresh()
+			} else {
+				log.Println("Error loading thumbnail:", err)
 			}
 
 			StatusText.SetText("Info fetched")
@@ -121,21 +150,17 @@ func main() {
 
 	downloadButton := widget.NewButton("Download", func() {
 		url := urlEntry.Text
-		selectedFormat := formatSelect.Selected
 		selectedResolution := resolutionSelect.Selected
 
-		if url == "" {
-			dialog.ShowInformation("Error", "Incorrect URL", myWindow)
-			return
-		} else if downloadPath == "" {
-			dialog.ShowInformation("Error", "Incorrect Download Path", myWindow)
+		if url == "" || selectedResolution == "" {
+			dialog.ShowInformation("Error", "Incorrect URL or resolution", myWindow)
 			return
 		}
 
 		Progress.SetValue(0)
 
 		go func() {
-			err := downloadVideo(url, downloadPath, selectedFormat, selectedResolution, progressChan)
+			err := downloadVideo(url, downloadPath, selectedResolution, progressChan)
 			if err != nil {
 				dialog.ShowError(err, myWindow)
 			} else {
@@ -162,7 +187,7 @@ func main() {
 	)
 
 	rightSide := container.NewVBox(
-		thumbnailImage, // Miniatura nad tytułem
+		thumbnailImage,
 		videoTitle,
 		separator,
 		scrollableDescription,
@@ -177,23 +202,24 @@ func main() {
 	myWindow.ShowAndRun()
 }
 
-func downloadVideo(url, downloadPath, format, resolution string, progressChan chan float64) error {
-	var args []string
-	if resolution == "Audio Only" {
-		args = append(args, "-f", "bestaudio")
-	} else {
-		args = append(args, "-f", "bestvideo[height="+resolution+"]+bestaudio")
+func downloadVideo(url, downloadPath, resolution string, progressChan chan float64) error {
+	args := []string{
+		"-f", fmt.Sprintf("bestvideo[height=%s]+bestaudio/best", resolution),
+		"-o", filepath.Join(downloadPath, "%(title)s.%(ext)s"),
+		"--no-check-certificate",
+		url,
 	}
-	args = append(args, "--ffmpeg-location", ffmpegPath, "-o", filepath.Join(downloadPath, "%(title)s.%(ext)s"), url)
-
 	cmd := exec.Command(ytDlp, args...)
+	log.Println("Running download command:", cmd)
+
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		return fmt.Errorf("Error starting download: %v", err)
+		return fmt.Errorf("error starting download: %v", err)
 	}
 
 	if err := cmd.Start(); err != nil {
-		return fmt.Errorf("Error starting download: %v", err)
+		log.Println("Error starting download:", err)
+		return fmt.Errorf("error starting download: %v", err)
 	}
 
 	scanner := bufio.NewScanner(stdout)
@@ -210,9 +236,11 @@ func downloadVideo(url, downloadPath, format, resolution string, progressChan ch
 	}
 
 	if err := cmd.Wait(); err != nil {
-		return fmt.Errorf("Error during download: %v", err)
+		log.Println("Download error:", err)
+		return fmt.Errorf("error during download: %v", err)
 	}
 
+	log.Println("Download completed successfully")
 	return nil
 }
 
@@ -225,7 +253,8 @@ func getDefaultDownloadPath() string {
 }
 
 func getVideoInfo(url string) (videoInfo, error) {
-	cmd := exec.Command(ytDlp, "-j", "--skip-download", url)
+	cmd := exec.Command(ytDlp, "-j", "--skip-download", "--no-warnings", "--no-check-certificate", "\"--user-agent\", \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36", url)
+	cmd.SysProcAttr = getOSSysProcAttr()
 	output, err := cmd.CombinedOutput()
 
 	if err != nil {
@@ -240,22 +269,54 @@ func getVideoInfo(url string) (videoInfo, error) {
 	return info, nil
 }
 
-func fetchThumbnail(url string) (*fyne.StaticResource, error) {
+func fetchAndConvertThumbnail(url string) (*fyne.StaticResource, error) {
 	response, err := http.Get(url)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error fetching thumbnail: %v", err)
 	}
 	defer response.Body.Close()
 
 	img, _, err := image.Decode(response.Body)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error decoding image: %v", err)
 	}
 
 	var buf bytes.Buffer
 	if err := jpeg.Encode(&buf, img, nil); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error encoding image to jpeg: %v", err)
 	}
 
 	return fyne.NewStaticResource("thumbnail.jpg", buf.Bytes()), nil
+}
+
+func getAvailableFormats(url string) ([]ytDlpFormat, error) {
+	cmd := exec.Command("yt-dlp", "-j", "--skip-download", "--format-sort", "res,br", url)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return nil, fmt.Errorf("Error while downloading video information: %v, %s", err, string(output))
+	}
+
+	var info videoInfo
+	if err := json.Unmarshal(output, &info); err != nil {
+		return nil, fmt.Errorf("Error while parsing information: %v", err)
+	}
+	return info.Formats, nil
+}
+
+// Funkcja wyciągająca unikalne rozdzielczości
+func getUniqueResolutions(formats []ytDlpFormat) []string {
+	resolutions := map[string]struct{}{}
+	for _, format := range formats {
+		if format.Resolution != "" {
+			resolutions[format.Resolution] = struct{}{}
+		}
+	}
+
+	sortedResolutions := make([]string, 0, len(resolutions))
+	for res := range resolutions {
+		sortedResolutions = append(sortedResolutions, res)
+	}
+
+	sort.Sort(sort.Reverse(sort.StringSlice(sortedResolutions)))
+	return sortedResolutions
 }
